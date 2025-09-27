4b6b9692cac016e1d3570da7e0dda10890a26fc4
aaljo2 <aaljo2@naver.com>
2025-09-27 14:37:46 +0900

    회로 이미지 추가

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,13 @@
 # utils/__init__.py
-from .anim import playbar, step_loop
+from . import style
+from .media import cache_remote, show_image, show_gif_cached
+
+# 더 이상 playbar는 필수 아님
+from .anim import step_loop  # 만 import
+
+# (선택) 과거 코드 호환용으로 playbar가 있으면 내보내고, 없으면 더미 제공
+try:
+    from .anim import playbar
+except Exception:
+    def playbar(*args, **kwargs):
+        return False
